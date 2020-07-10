5c35be2540d0705e6a265a2e98f73359e50505d1
natashashafqat <natashashafqat@gmail.com>
2020-07-10 14:43:32 +0100

    fix(): send response and add server;

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,6 @@
 FROM node:latest
 WORKDIR /app
-ADD index.js /app
-ADD package.json /app
+ADD . /app
 RUN npm install
 EXPOSE 3000
-CMD npm run start
+CMD npm start
--- index.js
+++ index.js
@@ -2,8 +2,12 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
+  res.send('Success! Your app is working.');
+});
+
+const server = app.listen(3000, () => {
   const host = server.address().address;
   const port = server.address().port;
 
-  console.log('Success! Your app is listening at http://%s:%s', host, port);
+  console.log('Your app is listening at http://%s:%s', host, port);
 });
